495d89c6f307c546b48a663a64400e28b7edde6c
Nesith2008 <nesithdbandara.gmail.com>
2023-06-24 09:08:39 +0530

    added js-demo

--- js/main.js
+++ js/main.js
@@ -11,8 +11,120 @@ function print() {
 print();
 
 function getData() {
-    var email = document.getElementById('email').value;
-    document.getElementById('result').innerHTML=email;
-    console.log(email);
-
+    var email = parseInt (document.getElementById('email').value);
+    document.getElementById('result').innerHTML=+email+5;
+    console.log(email+5);
 }
+
+var mark1=75;
+    var mark2=40;
+    var mark3=15;
+    var avg=(mark1+mark2+mark3)/3;
+    if (avg>=75) {
+        console.log("A");
+    } else if(avg>=65) {
+        console.log("B");
+    } else if(avg>=55) {
+        console.log("C");
+    } else if(avg>=35) {
+        console.log("D");
+    }else {
+        console.log("F");
+    }
+
+    var stdName = 67;
+
+    switch(stdName){
+        case 78:
+            console.log("Hi yasindu");
+            break;
+        case "kavindu":
+            console.log("Hi kavindu");
+            break;
+        case "pasindu":
+            console.log("Hi pasindu");
+            break;
+        default:
+            console.log("Not mached !");
+    }
+
+   
+
+  var month ="March";
+
+     switch(month){
+      case "January":
+        console.log("Duruthu");
+        break;
+      case "February":
+        console.log("Nawam");
+        break;
+      case"March":
+        console.log("Medin");
+        break;
+      default:
+        console.log("not match");
+    
+     }
+
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+     console.log("Hi Dulranga");
+
+     for(var i=0; i<10; i++){
+        console.log("Hi kavindu");
+     }
+     function getAlert(){
+        var inputValue = document.getElementById('num').value;
+        for(i=1;i<6;i++){
+            inputValue *=i;
+        }
+        if((inputValue%2)==0){
+            alert("user win");
+        }else{
+            alert("user failed");
+        }
+    }
+
+
+  
+    while (false) {
+        
+    }
+
+    var cars = ("Toyota","BMW","Benz","AUDI","Jeep",89,56);
+     console.log(cars[0]);
+
+     console.log(cars);
+
+     var stdName ="kavindu";
+     stdName=90 ;
+
+     for (var val of cars) {
+
+        console.log(val);
+        
+     }
+
+     var rainData = [34,65,7,65,3,23,5,78,9,90,78];
+     var total =0;
+     for(var x of rainData){
+     total+=x;
+     }
+     var avg = total/11;
+     console.log(avg);
+
+     var total=0;
+     for(var i=0; i<rainData.length; i++){
+     total += rainData[i];
+    }
+     console.log(total/rainData.length);
